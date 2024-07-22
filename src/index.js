b51faa29222afbc6100c5593de89559a69aecd35
Arlan Galvez <galvezalonso@gmail.com>
2024-07-22 16:13:15 -0400

    feat: init repo with base react app

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from 'react-dom/client';
+
+const rootElement = document.getElementById('root');
+
+const root = ReactDOM.createRoot(rootElement);
+
+root.render(
+    <div>
+        <h1>Hello World</h1>
+        <p>This is a paragraph</p>
+    </div>
+);
