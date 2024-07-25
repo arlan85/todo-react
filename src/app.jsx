f2687344e3bf9295713966f85e0cf7fbc24c48dc
Arlan Galvez <galvezalonso@gmail.com>
2024-07-25 18:46:38 -0400

    feat: add more content to properties and events

--- src/app.jsx
+++ src/app.jsx
@@ -1,5 +1,42 @@
-import React from "react";
+import React, { Fragment, useRef, useState } from "react";
+import { v4 as uuidv4 } from "uuid";
+import { TodoList } from "./components/TodoList";
 
 export function App(){
-  return (<div>HOLA MUNDO!!!!</div>)
+  const [todos, setTodos] = useState([{id: uuidv4(), task: "tarea 1", completed: false}]);
+  const todoTaskRef = useRef();
+  
+  const handleTodoAdd = () => {
+    const tarea = todoTaskRef.current.value;
+    if(tarea === "") return;
+
+    const newTodo = {
+      id: uuidv4(),
+      task: tarea,
+      completed: false
+    }
+
+    setTodos((prevTodos)=>{
+      return [...prevTodos, newTodo]
+    })
+
+    todoTaskRef.current.value = "";
+    // setTodos([...todos, newTodo])
+  }
+
+  const toggleTodo = (id) => {
+    const newTodos = [...todos];
+    const todo = newTodos.find(todo => todo.id === id);
+    todo.completed = !todo.completed;
+    setTodos(newTodos);
+  }
+
+  return (
+  <Fragment>
+    <TodoList todos={todos} toggleTodo={toggleTodo}/> 
+    <input type="text" ref={todoTaskRef} placeholder="Nueva tarea" />
+    <button onClick={handleTodoAdd}>Add</button>
+    <button>Delete</button>
+  </Fragment>
+)
 }
